aeb618fdc80ff27fc7750461a2e4200f3f2331b6
krapa <251745@astanait.edu.kz>
2025-12-10 20:37:52 +0500

    first

---
+++ src/com/krapa/clothingstore/ClothingItem.java
@@ -0,0 +1,5 @@
+package com.krapa.clothingstore;
+
+public class ClothingItem{
+
+}
---
+++ src/com/krapa/clothingstore/Main.java
@@ -0,0 +1,15 @@
+package com.krapa.clothingstore;
+public class Main{ // main class
+    public static void main(String[] args){
+        System.out.println("===Clothing Store Management System==="); // Welcome message
+        System.out.println();
+        System.out.println("This program's purposes are to:");
+        System.out.println("Record and store, manage all data about clothing store such as:");
+        System.out.println("-Clothes inventory: items, sizes, brands");
+        System.out.println("-Customers and employee information");
+        System.out.println("-Orders information");
+        System.out.println();
+        System.out.println("Name: Kira");
+        System.out.println("Course: IT-2505");
+    }
+}
